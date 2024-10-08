54cf0f00dd0b2c239c9f5f4c6f469887a2ff6960
Baines Medgar <bainesme@si-c1.nsk.21-school.ru>
2024-10-08 16:12:40 +0700

    add test branch

--- eventory/main/models.py
+++ eventory/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    last_name = models.CharField('last_name', max_length=50)
+    first_name = models.CharField('first_name', max_length=50)
+    phone = models.CharField('phone', max_length=15)
+    email = models.CharField('email', max_length=50)
+    password = models.CharField('password', max_length=50)
+
+    def __str__(self):
+        return self.last_name
+
+
+
--- eventory/main/views.py
+++ eventory/main/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import Users
 
 
 def index(request):
+    user = Users.objects.create(
+        last_name='Ivanov',
+        first_name='Ivan',
+        phone='+79538794883',
+        email='alexeyshu86@mail.ru',
+        password='123'
+    )
+
     return render(request, 'main/index.html')
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
