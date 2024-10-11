c80d31c3953f7e167e986215270291992e3f83c9
bainesme <alexeyshu86@mail.ru>
2024-10-12 01:26:34 +0700

    add CustomUser in registration.model

--- eventory/registration/views.py
+++ eventory/registration/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import CustomUser
 
 
 def user_registration(request):
+    # user = CustomUser.objects.create_user(
+    #     username = 'probe@m.ru',
+    #     email = 'probe@m.ru',
+    #     password = 'my_password',
+    #     first_name = 'Alexey',
+    #     last_name = 'Shushakov',
+    #     phone = '89538794883'
+    # )
     return render(request, 'registration/user_regs.html')
