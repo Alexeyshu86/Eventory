9dbf350b800b38beee2c54c35b124df68b453af3
Doduo Fortune <doduofor@si-c3.nsk.21-school.ru>
2024-10-12 18:26:43 +0700

    add css for calendar

--- eventory/my_calendar/urls.py
+++ eventory/my_calendar/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calendar/', views.my_calendar, name='my_calendar')
+    path('calendar/', views.my_calendar, name='my_calendar'),
+    path('test_calendar/', views.my_test_calendar, name='my_test_calendar')
 ]
