54a3b81f5013f7e6b55a07391179e55e4b4b1ac4
bainesme <alexeyshu86@mail.ru>
2024-10-15 22:11:16 +0700

    add req.meth==POST

--- eventory/registration/views.py
+++ eventory/registration/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import CustomUser, Interest
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
 
 def get_interests():
@@ -17,4 +19,14 @@ def user_registration(request):
 
     interest_list = get_interests()
 
-    return render(request, 'registration/user_regs.html', {'interest_list': interest_list})
+    if request.method == 'POST':
+        surname = request.POST.get('surname')
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        list_interest = request.POST.getlist('tags[]')
+        return HttpResponse(f'name: {name}\nsurname: {surname}\n'
+                            f'phone: {phone}\nemail: {email}\n'
+                            f'interests: {list_interest}')
+    else:
+        return render(request, 'registration/user_regs.html', {'interest_list': interest_list})
