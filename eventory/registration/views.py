ddabf8329a8dcb26751fcc28fe69beffd8121a60
bainesme <alexeyshu86@mail.ru>
2024-10-16 09:08:23 +0700

    user add in table

--- eventory/registration/views.py
+++ eventory/registration/views.py
@@ -1,32 +1,36 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import CustomUser, Interest
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 
-
-def get_interests():
-    return Interest.objects.all()
 
 def user_registration(request):
-    # user = CustomUser.objects.create_user(
-    #     username = 'probe@m.ru',
-    #     email = 'probe@m.ru',
-    #     password = 'my_password',
-    #     first_name = 'Alexey',
-    #     last_name = 'Shushakov',
-    #     phone = '89538794883'
-    # )
 
-    interest_list = get_interests()
+    error_passw_repeat = None
+    interest_list = Interest.objects.all()
 
     if request.method == 'POST':
-        surname = request.POST.get('surname')
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-        list_interest = request.POST.getlist('tags[]')
-        return HttpResponse(f'name: {name}\nsurname: {surname}\n'
-                            f'phone: {phone}\nemail: {email}\n'
-                            f'interests: {list_interest}')
+        t_surname = request.POST.get('surname')
+        t_name = request.POST.get('name')
+        t_phone = request.POST.get('phone')
+        t_email = request.POST.get('email')
+        t_list_interest = request.POST.getlist('tags[]')
+        t_password = request.POST.get('password')
+        t_password_repeat = request.POST.get('password_repeat')
+        if t_password_repeat != t_password:
+            error_passw_repeat = 'Пароли не совпадают.'
+            return render(request, 'registration/user_regs.html',
+                          {'interest_list': interest_list, 'error_passw_repeat': error_passw_repeat})
+        try:
+            user = CustomUser.objects.create_user(
+                username=t_email,
+                email=t_email,
+                password=t_password,
+                first_name=t_name,
+                last_name=t_surname,
+                phone=t_phone
+            )
+            return redirect('home')
+        except Exception as e:
+            return HttpResponse(f'Ошибка при создании пользователя: {str(e)}')
     else:
-        return render(request, 'registration/user_regs.html', {'interest_list': interest_list})
+        return render(request, 'registration/user_regs.html',
+                      {'interest_list': interest_list, 'error_passw_repeat': error_passw_repeat})
