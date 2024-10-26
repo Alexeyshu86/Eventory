38c5ed48494f47ffe49e7a8aa11229d68ccffe79
Baines Medgar <bainesme@si-c10.nsk.21-school.ru>
2024-10-26 19:41:56 +0700

    change password

--- eventory/cabinet/views.py
+++ eventory/cabinet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth import logout
 from registration.models import CustomUser, Interest
 
@@ -12,36 +13,66 @@ def cabinet(request):
     return render(request, 'cabinet/cabinet.html', context)
 
 
-def change_data(request):
-    user = CustomUser.objects.get(id=request.session.get('user_id'))
-    interest_list = Interest.objects.all()
+def update_data(custom_user_model, interest_model, user_id):
+    user = custom_user_model.objects.get(id=user_id)
+    interest_list = interest_model.objects.all()
     context = {
         'user': user,
         'interest_list': interest_list,
     }
-    # number = Person.objects.filter(id=1).update(name="Mike")
+    return context
 
-    if request.method == 'POST':
+
+def update_password(old_password, new_password, user_id):
+    user = CustomUser.objects.get(id=user_id)
+    if check_password(old_password, user.password):
+        user.password = make_password(new_password)
+        user.save()
+    else:
+        return HttpResponse('Введите правильный пароль')
+
+
+def change_data(request):
+
+    user = CustomUser.objects.get(id=request.session.get('user_id'))
+    context = update_data(CustomUser, Interest, user.id)
+
+    if request.method == 'GET':
+        return render(request, 'cabinet/change_data.html', context)
+
+    elif request.method == 'POST':
         first_name = request.POST.get('name')
         if first_name:
-            first_name = CustomUser.objects.filter(id=user.id).update(first_name=first_name)
+            CustomUser.objects.filter(id=user.id).update(first_name=first_name)
 
         last_name = request.POST.get('surname')
         if last_name:
-            last_name = CustomUser.objects.filter(id=user.id).update(last_name=last_name)
+            CustomUser.objects.filter(id=user.id).update(last_name=last_name)
 
         phone = request.POST.get('phone')
         if phone:
-            phone = CustomUser.objects.filter(id=user.id).update(phone=phone)
+            CustomUser.objects.filter(id=user.id).update(phone=phone)
 
         email = request.POST.get('email')
         if email:
-            email = CustomUser.objects.filter(id=user.id).update(email=email)
-            username = CustomUser.objects.filter(id=user.id).update(username=email)
+            CustomUser.objects.filter(id=user.id).update(email=email)
+            CustomUser.objects.filter(id=user.id).update(username=email)
 
-        return HttpResponse(f'name: {first_name}\nlastname: {last_name}\nphone: {phone}\nemail: {email}')
-    elif request.method == 'GET':
-        return render(request, 'cabinet/change_data.html', context)
+        interests = request.POST.getlist('tags[]')
+        if interests:
+            CustomUser.objects.filter(id=user.id).update(interests=interests)
+
+        old_password = request.POST.get('old_password')
+        new_password = request.POST.get('new_password')
+        if old_password and new_password:
+            update_password(old_password, new_password, user.id)
+        elif old_password and new_password == '':
+            return HttpResponse('Введите новый пароль')
+        elif old_password == '' and new_password:
+            return HttpResponse('Введите старый пароль')
+
+        return redirect('change_data')
+
     else:
         return JsonResponse({'success': False, 'error': 'Invalid change data'})
 
